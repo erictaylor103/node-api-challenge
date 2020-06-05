e76795a369363d938e981f83844b747ae533a3af
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2020-06-05 12:44:14 -0400

    GET requests created for actionsRouter and projectsRouter

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -3,7 +3,21 @@ const Actions = require('../data/helpers/actionModel.js');
 const router = express.Router();
 
 
+//GET all actions
 
+router.get('/', function(req, res){
+    
+    Actions.get(res.body)
+    .then(actions => {
+        console.log(actions);
+        res.status(200).json(actions);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({ error: "Could not get actions" })
+        
+    })
+})
 
 
 
--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -4,7 +4,19 @@ const Projects = require('../data/helpers/projectModel.js');
 const router = express.Router();
 
 
-
+//GET all projects
+router.get('/', function(req, res){
+    Projects.get(res.body)
+    .then(project => {
+        console.log(project);
+        res.status(200).json(project);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({ error: "The Projects information could not be retrieved" })
+        
+    })
+});
 
 
 
