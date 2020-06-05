36e008ea0bf86c807bcb8e0b1ca427e3f97b31a5
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2020-06-05 12:41:26 -0400

    Created the routers folder - set up actionsRouter.js and projectsRouter.js files

---
+++ routers/projectsRouter.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const Actions = require('../data/helpers/actionModel.js');
+const Projects = require('../data/helpers/projectModel.js');
+const router = express.Router();
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
